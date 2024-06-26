54404a1b3c49ff9997b0bc6a2e4b897cf615563e
ohkyungmin <ohkm9926@naver.com>
2024-06-26 16:50:12 +0900

    commit

--- js/start.js
+++ js/start.js
@@ -19,7 +19,7 @@ function setResult(){
 
     var resultImg = document.createElement('img');
     const imgDiv = document.querySelector('#resultImg');
-    var imgURL = 'img/image-' + point + '.png';
+    var imgURL = 'img/image-' + point + '.ico';
     resultImg.src = imgURL;
     resultImg.alt = point;
     resultImg.classList.add('img-fluid');
